8c4bfb05e176ec50fcdeadd428a767e2ade8eff2
Bartosz_Pingot <pingotbartosz13@gmail.com>
2018-11-27 16:58:13 +0100

    Adding register form and conting keyboard pressing characteristics

--- KeyboardBiometrics/Form1.Designer.cs
+++ KeyboardBiometrics/Form1.Designer.cs
@@ -29,74 +29,58 @@
         private void InitializeComponent()
         {
             this.mainPanel = new System.Windows.Forms.Panel();
-            this.savePatternButton = new System.Windows.Forms.Button();
-            this.validatePatternButton = new System.Windows.Forms.Button();
-            this.savePatternPanel = new System.Windows.Forms.Panel();
-            this.validatePanel = new System.Windows.Forms.Panel();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
             this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
             this.mainPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.mainPanel.Controls.Add(this.validatePatternButton);
-            this.mainPanel.Controls.Add(this.savePatternButton);
+            this.mainPanel.Controls.Add(this.registerButton);
+            this.mainPanel.Controls.Add(this.loginButton);
             this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanel.Location = new System.Drawing.Point(0, 0);
             this.mainPanel.Name = "mainPanel";
             this.mainPanel.Size = new System.Drawing.Size(800, 450);
             this.mainPanel.TabIndex = 0;
             // 
-            // savePatternButton
+            // registerButton
             // 
-            this.savePatternButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.savePatternButton.Location = new System.Drawing.Point(59, 93);
-            this.savePatternButton.Name = "savePatternButton";
-            this.savePatternButton.Size = new System.Drawing.Size(290, 271);
-            this.savePatternButton.TabIndex = 0;
-            this.savePatternButton.Text = "Save Pattern";
-            this.savePatternButton.UseVisualStyleBackColor = true;
-            this.savePatternButton.Click += new System.EventHandler(this.savePatternButton_Click);
+            this.registerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.registerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.registerButton.Location = new System.Drawing.Point(252, 275);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(290, 82);
+            this.registerButton.TabIndex = 1;
+            this.registerButton.Text = "Sign up";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
             // 
-            // validatePatternButton
+            // loginButton
             // 
-            this.validatePatternButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.validatePatternButton.Location = new System.Drawing.Point(452, 93);
-            this.validatePatternButton.Name = "validatePatternButton";
-            this.validatePatternButton.Size = new System.Drawing.Size(290, 271);
-            this.validatePatternButton.TabIndex = 1;
-            this.validatePatternButton.Text = "Validate";
-            this.validatePatternButton.UseVisualStyleBackColor = true;
-            this.validatePatternButton.Click += new System.EventHandler(this.validatePatternButton_Click);
-            // 
-            // savePatternPanel
-            // 
-            this.savePatternPanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.savePatternPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.savePatternPanel.Location = new System.Drawing.Point(0, 0);
-            this.savePatternPanel.Name = "savePatternPanel";
-            this.savePatternPanel.Size = new System.Drawing.Size(800, 450);
-            this.savePatternPanel.TabIndex = 2;
-            this.savePatternPanel.Visible = false;
-            // 
-            // validatePanel
-            // 
-            this.validatePanel.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.validatePanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.validatePanel.Location = new System.Drawing.Point(0, 0);
-            this.validatePanel.Name = "validatePanel";
-            this.validatePanel.Size = new System.Drawing.Size(800, 450);
-            this.validatePanel.TabIndex = 3;
-            this.validatePanel.Visible = false;
+            this.loginButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.loginButton.Location = new System.Drawing.Point(252, 107);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(290, 82);
+            this.loginButton.TabIndex = 0;
+            this.loginButton.Text = "Sign in";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
             // 
             // KeyboardBiometrics
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.savePatternPanel);
-            this.Controls.Add(this.validatePanel);
             this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "KeyboardBiometrics";
             this.Text = "KeyboardsBiometrics";
             this.mainPanel.ResumeLayout(false);
@@ -107,10 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Panel mainPanel;
-        private System.Windows.Forms.Button validatePatternButton;
-        private System.Windows.Forms.Button savePatternButton;
-        private System.Windows.Forms.Panel savePatternPanel;
-        private System.Windows.Forms.Panel validatePanel;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button loginButton;
     }
 }
 
---
+++ KeyboardBiometrics/loginForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace KeyboardBiometrics
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonback = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonback
+            // 
+            this.buttonback.Location = new System.Drawing.Point(12, 12);
+            this.buttonback.Name = "buttonback";
+            this.buttonback.Size = new System.Drawing.Size(102, 43);
+            this.buttonback.TabIndex = 0;
+            this.buttonback.Text = "Back";
+            this.buttonback.UseVisualStyleBackColor = true;
+            this.buttonback.Click += new System.EventHandler(this.buttonback_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonback);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonback;
+    }
+}
